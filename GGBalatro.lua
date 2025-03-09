895e2e0883e0612d1a15317f6742b04d67e1ffcc
Waspsting <llfeimi@outlook.com>
2025-03-08 23:26:59 -0500

    started adding faust but crashes when effect is triggered

--- GGBalatro.lua
+++ GGBalatro.lua
@@ -1,18 +1,3 @@
---[[
-------------------------------Basic Table of Contents------------------------------
-Line 17, Atlas ---------------- Explains the parts of the atlas.
-Line 29, Joker 2 -------------- Explains the basic structure of a joker
-Line 88, Runner 2 ------------- Uses a bit more complex contexts, and shows how to scale a value.
-Line 127, Golden Joker 2 ------ Shows off a specific function that's used to add money at the end of a round.
-Line 163, Merry Andy 2 -------- Shows how to use add_to_deck and remove_from_deck.
-Line 207, Sock and Buskin 2 --- Shows how you can retrigger cards and check for faces
-Line 240, Perkeo 2 ------------ Shows how to use the event manager, eval_status_text, randomness, and soul_pos.
-Line 310, Walkie Talkie 2 ----- Shows how to look for multiple specific ranks, and explains returning multiple values
-Line 344, Gros Michel 2 ------- Shows the no_pool_flag, sets a pool flag, another way to use randomness, and end of round stuff.
-Line 418, Cavendish 2 --------- Shows yes_pool_flag, has X Mult, mainly to go with Gros Michel 2.
-Line 482, Castle 2 ------------ Shows the use of reset_game_globals and colour variables in loc_vars, as well as what a hook is and how to use it.
---]]
-
 --Creates an atlas for cards to use
 SMODS.Atlas {
 	-- Key for code to find it with
@@ -33,26 +18,58 @@ SMODS.Joker {
 	loc_txt = {
 		name = 'John',
 		text = {
-			"{C:mult}+#1# {} Mult"
+			"{X:mult,C:white}x#1# {} Mult"
 		}
 	},
-	config = { extra = { mult = 4 } },
+	config = { extra = { Xmult = 69 } },
 	loc_vars = function(self, info_queue, card)
-		return { vars = { card.ability.extra.mult } }
+		return { vars = { card.ability.extra.Xmult } }
 	end,
 	rarity = 4,
 	atlas = 'GGBalatro',
 	pos = { x = 0, y = 0 },
-	cost = 100,
+	cost = 999,
 	calculate = function(self, card, context)
 		if context.joker_main then
 			return {
-				mult_mod = card.ability.extra.mult,
-				message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } }
+				mult_mod = card.ability.extra.Xmult,
+				message = localize { type = 'variable', key = 'X_mult', vars = { card.ability.extra.Xmult } }
 			}
 		end
 	end
 }
 
+SMODS.Joker {
+  key = 'faust',
+  loc_txt = {
+    name = 'Faust',
+    text = {
+      "When {C:attention}blind{} is selected,",
+      "create a random {C:dark_edition}Negative{}",
+      "{C:attention}consumable{} card"
+    }
+  },
+  config = { extra = {} },
+  rarity =4,
+  atlas = 'GGBalatro',
+  pos = { x = 0, y = 0},
+  cost = 999,
+  loc_vars = function(self, info_queue, card)
+    info_queue[#info_queue+1] = G.P_CENTERS.e_negative
+  end,
+  calculate = function(self, card, context)
+    if context.setting_blind then
+      G.E_MANAGER:add_event(Event({
+        SMODS.create_card(
+          {
+            set = 'Planet', 'Tarot', 'Spectral',
+            area = 'G.consumables',
+            soulable = 'true',
+            edition = 'negative'
+          }),
+      }))
+    end
+  end
+}
 ----------------------------------------------
 ------------MOD CODE END----------------------
