56464dc214f088a9bb35384912dc64deef6d1ff0
Waspsting <llfeimi@outlook.com>
2025-03-20 21:17:16 -0400

    ky kiske basic implementation

--- GGBalatro.lua
+++ GGBalatro.lua
@@ -6,30 +6,4 @@ SMODS.load_file("spells.lua")()
 SMODS.load_file("faust_items.lua")()
 SMODS.load_file("jokers.lua")()
 
-SMODS.Joker {
-  key = 'axl',
-  loc_txt = {
-    name = 'Axl Low',
-    text = {"WHAT DOES HE EVEN {C:mult}DO?{}"}
-  },
-  config = { extra = {} },
-  atlas = 'axl_at',
-  pos = { x = 0, y = 0},
-  rarity = 'ggbt_blazing',
-  cost = 999,
-  calculate = function(self, card, context)
-    if context.setting_blind then
-      G.E_MANAGER:add_event(Event({
-        trigger = 'after',
-        delay = 0.3,
-        blockable = false,
-        func = function()
-          G.jokers:remove_card(card)
-          card:remove()
-          card = nil
-          return true;
-        end
-      }))
-    end
-  end
-}
+
