10481f1f1e9c4630ea88fa69e194d33299e25999
Waspsting <llfeimi@outlook.com>
2025-03-24 20:43:21 -0400

    .

--- GGBalatro.lua
+++ GGBalatro.lua
@@ -6,3 +6,4 @@ SMODS.load_file("spells.lua")()
 SMODS.load_file("faust_items.lua")()
 SMODS.load_file("jokers.lua")()
 
+
--- jokers.lua
+++ jokers.lua
@@ -75,15 +75,15 @@ SMODS.Joker {
   loc_txt = {
     name = 'Ky Kiske',
     text = {
-      "Gains {X:chips,C:white}X0.5{} Chips if",
+      "Gains {X:chips,C:white}X#2#{} Chips if",
       "played hand contains",
       "exactly {C:attention}3 Kings{}",
       "{C:inactive}(Currently {X:chips,C:white}X#1#{C:inactive} Chips)"
     }
   },
-  config = { extra = { Xchip = 1, Xchip_gain = 0.5 } },
+  config = { extra = { Xchip = 1, Xchip_gain = 0.50 } },
   loc_vars = function(self, info_queue, card)
-    return { vars = { card.ability.extra.Xchip, card.ability.extra.chip_gain } }
+    return { vars = { card.ability.extra.Xchip, card.ability.extra.Xchip_gain } }
   end,
   atlas = 'GGBalatro',
   pos = { x = 0, y = 0},
@@ -93,7 +93,7 @@ SMODS.Joker {
     if context.joker_main then
       if card.ability.extra.Xchip > 1 then
         return {
-          message = localize { type = 'variable', key= 'a_xchips', vars = { card.ability.extra.Xchip } },
+          message = localize { type = 'variable', key= 'a_xchip', vars = { card.ability.extra.Xchip } },
           Xchip_mod = card.ability.extra.Xchip
         }
       end
@@ -108,8 +108,56 @@ SMODS.Joker {
       if count == 3 then
         card.ability.extra.Xchip = card.ability.extra.Xchip + card.ability.extra.Xchip_gain
         return {
-          message = "Upgraded!", -- Second message doesnt show
+          message = "Upgraded!",
           colour = G.C.CHIPS,
+          card = card
+        }
+      end
+    end
+  end
+}
+
+
+SMODS.Joker {
+  key = 'daryl',
+  loc_txt = {
+    name = 'Daryl',
+    text = {
+      "Gains {X:mult,C:white}X#2#{} Mult if",
+      "played hand contains",
+      "exactly {C:attention}3 Kings{}",
+      "{C:inactive}(Currently {X:mult,C:white}X#1#{C:inactive} Mult)"
+    }
+  },
+  config = { extra = { Xmult = 1, Xmult_gain = 0.25 } },
+  loc_vars = function(self, info_queue, card)
+    return { vars = { card.ability.extra.Xmult, card.ability.extra.Xmult_gain } }
+  end,
+  atlas = 'GGBalatro',
+  pos = { x = 0, y = 0},
+  rarity = 'ggbt_blazing',
+  cost = 999,
+  calculate = function(self, card, context)
+    if context.joker_main then
+      if card.ability.extra.Xmult > 1 then
+        return {
+          message = localize { type = 'variable', key= 'a_xmult', vars = { card.ability.extra.Xmult } },
+          Xmult_mod = card.ability.extra.Xmult
+        }
+      end
+    end
+    if context.before and not context.blueprint then
+      local count = 0
+      for _, _card in pairs(G.play.cards) do
+        if _card:get_id() == 13 then
+          count = count + 1
+        end
+      end
+      if count == 3 then
+        card.ability.extra.Xmult = card.ability.extra.Xmult + card.ability.extra.Xmult_gain
+        return {
+          message = "Upgraded!",
+          colour = G.C.MULT,
           card = card
         } 
       end
@@ -117,4 +165,49 @@ SMODS.Joker {
   end
 }
 
+  SMODS.Joker {
+  key = 'leo',
+  loc_txt = {
+    name = 'Leo Whitefang', 
+    text = {
+      "Earn {C:money}#1#${} at end of round",
+      "payout increases by {C:money}#2#${} when",
+      "played hand contains exactly {C:attention}3 Kings{}"
+    }
+  },
+  config = { extra = { payout = 1, payout_gain = 0.50 } },
+  loc_vars = function(self, info_queue, card)
+    return { vars = { card.ability.extra.payout, card.ability.extra.payout_gain } }
+  end,
+  atlas = 'GGBalatro',
+  pos = { x = 0, y = 0},
+  rarity = 'ggbt_blazing',
+  cost = 999,
+  calculate = function(self, card, context)
+    if context.joker_main then
+      if card.ability.extra.payout > 0 then
+        return {
+          message = localize { type = 'variable', key= 'a_xmult', vars = { card.ability.extra.Xmult } },
+          Xmult_mod = card.ability.extra.Xmult
+        }
+      end
+    end
+    if context.before and not context.blueprint then
+      local count = 0
+      for _, _card in pairs(G.play.cards) do
+        if _card:get_id() == 13 then
+          count = count + 1
+        end
+      end
+      if count == 3 then
+        card.ability.extra.Xmult = card.ability.extra.Xmult + card.ability.extra.Xmult_gain
+        return {
+          message = "Upgraded!",
+          colour = G.C.MULT,
+          card = card
+        } 
+      end
+    end
+  end
+}
 
